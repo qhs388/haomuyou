d5632f9ef42a1aec2cd5188d0ac6672b7834f21d
liukang <2549974870@qq.com>
2021-02-19 18:42:40 +0800

    时间

---
+++ haomuyou/components/yd-classification/yd-classification.vue
@@ -0,0 +1,568 @@
+<template>
+	<view class="classification">
+		<view class="left">
+			<scroll-view class="leftScroll" style="height: 100vh;" :scroll-top="scrollTop" scroll-y="true">
+				<view :class="{type1:true, current:(index1 === i)}" :style="{lineHeight: type1LineHeight, fontSize: type1Size, height: type1Height, color: selected(i)}" v-for="(item,i) in list" :key="i" @click="changeType1(item,i)">{{item.name}}</view>
+			</scroll-view>
+		</view>
+		<view class="right">
+			<scroll-view class="rightScroll" style="height: 100vh;" :scroll-top="scrollTop" scroll-y="true">
+				<view class="type2" v-for="(item,i) in data2" :key="i">
+					<p :style="{fontSize: type2Size}">{{ item.name }}</p>
+					<view class="type3Box">
+						<view class="type3" v-for="(item1,i1) in item.child" :key="i1" @click="selectType1(i,i1)">
+							<image class="icon" mode="aspectFill" lazy-load="true" :src="item1.icon"></image>
+							<text :style="{fontSize: type3Size}">{{item1.name}}</text>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			index: {
+				type: Number,
+				default: 0
+			},
+			list: {
+				type: Array,
+				default: [
+					{
+						name: "家装",
+						child: [
+							{
+								name: "卧室",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室1",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室2",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室3",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室4",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室6",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室7",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室8",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							}
+						]
+					},
+					{
+						name: "家装1",
+						child: [
+							{
+								name: "卧室",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室1",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室2",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室3",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室4",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室6",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室7",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							},
+							{
+								name: "卧室8",
+								child: [
+									{
+										name: "台灯",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									},
+									{
+										name: "台灯1",
+										icon: {
+											type: String,
+											default: "https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
+										}
+									}
+								]
+							}
+						]
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					},
+					{
+						name: "家装2",
+						child: []
+					}
+				]
+			},
+			selectedColor: {
+				type: String,
+				default: "#ff852a"
+			},
+			type1Size: {
+				type: String,
+				default: "14px"
+			},
+			type2Size: {
+				type: String,
+				default: "14px"
+			},
+			type3Size: {
+				type: String,
+				default: "10px"
+			},
+			type1Height: {
+				type: String,
+				default: "90rpx"
+			},
+			type1LineHeight: {
+				type: String,
+				default: "90rpx"
+			}
+		},
+		data() {
+			return {
+				scrollTop: 0,
+				data1: [], // 用来存储传来的数据
+				index1: 0, // 标识当前选中的type1
+				data2: [], // 用来存储当前选中的type2数据
+				color: "", // 用来存储选中type1的颜色
+				load: 0 // 用来判断是否是第一次加载数据
+			}
+		},
+		mounted() {
+			
+		},
+		beforeUpdate() {
+			if(this.load == 0){
+				// 判断是不是首次加载
+				// 如果是将传入的值初始化到本地
+				this.index1 = this.index
+				this.data1 = this.list
+				this.color = this.selectedColor
+				console.log("这个时候的数据",this.list)
+				this.changeType1(this.data1[this.index1],this.index1)
+				this.load++
+			}
+			// 如果不是则不执行操作				
+		},
+		methods: {
+			changeType1(item,index) {
+				console.log("现在的index",index)
+				this.index1 = index
+				if (item === undefined ||item === null || item.child === undefined || item.child === null){
+					console.log("该内容为空！")
+					this.data2 = []
+				} else
+					this.data2 = item.child				
+			},
+			selectType1(i, i1) {
+				this.$emit("selectType", this.index1, i, i1)
+			},
+			// 更换颜色
+			selected: function(val){
+				return (val === this.index1?this.selectedColor+" !important":"")
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.classification {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		height: 100%;
+		background-color: #F8F8F8;
+		
+		/* 隐藏滚动条 */
+		::-webkit-scrollbar {  
+			display: none;  
+			width: 0 !important;  
+			height: 0 !important;  
+			-webkit-appearance: none;  
+			background: transparent;  
+		}
+		.left {
+			width: 160rpx;
+			background-color: #fff;
+			.type1 {
+				width: 158rpx;
+				height: 90rpx;
+				line-height: 90rpx;
+				text-align: center;
+				font-size: 14px;
+			}
+			.current {
+				color: #ff852a;
+				background-color: #F8F8F8;
+			}
+		}
+		.right {
+			float: right;
+			width: 520rpx;
+			margin-right: 30rpx;
+			
+			.type2 {
+				p {
+					margin: 46rpx 0 26rpx 0;
+					font-size: 14px;
+				}
+				.type3Box {
+					width: 100%;
+					height: auto;
+					border-radius: 28rpx;
+					background-color: #fff;
+					display: flex;
+					flex-wrap: wrap;
+					padding-top: 36rpx;
+					margin-bottom: 36rpx;
+					.type3 {
+						margin: 0 36rpx 36rpx 36rpx;
+						.icon {
+							width: 100rpx;
+							height: 100rpx;
+							display: block;
+						}
+						text {
+							padding: 18rpx 0;
+							display: block;
+							width: 100rpx;
+							font-size: 10px;
+							overflow: hidden;
+							text-align: center;
+						}
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ haomuyou/index/goodsDetail/goodsDetail.vue
@@ -0,0 +1,256 @@
+<template>
+	<view>
+		<view  style="padding-bottom: 100upx;box-sizing: border-box;">
+			<u-swiper :list="list" height="600" indicator-pos="bottomRight" mode="number"></u-swiper>
+			<view class="wrapper">
+				<view class="share">
+					<view class="money">
+						<text style="font-size: 30upx;">￥</text>
+						<text>3666.00</text>
+					</view>
+					<view class="vip-money">
+						￥3666.00
+					</view>
+					<view class="vip-icon">
+						<image src="/static/img/sqcwvip.png" mode=""></image>
+					</view>
+				</view>
+				<view class="introduce">
+					A1113 十二生肖（巴西花梨）
+				</view>
+				<view class="label">
+					<view>
+						原价：￥4032.60
+					</view>
+					<view>
+						库存：99件
+					</view>
+					<view>
+						销量：2件
+					</view>
+				</view>
+			</view>
+			<view class="line"></view>
+			<view class="attribute">
+				<view class="attribute-left">
+					请选择：
+				</view>
+				<u-icon name="arrow-right" color="#82848f"></u-icon>
+			</view>
+			<view class="line"></view>
+			<view class="UserEvaluation">
+				<view class="UserEvaluation-left">
+					用户评价(0)
+				</view>
+				<view class="UserEvaluation-right">
+					<view >
+						<text style="color: #E34819 ;">0.00%</text>
+					</view>
+					<view style="margin-left: 10upx;">
+						<text>好评率</text>
+					</view>
+					<u-icon name="arrow-right" color="#82848f"></u-icon>
+				</view>
+			</view>
+			<view class="line"></view>
+			<view class="shop-detail">
+				<view class="title">
+					产品介绍
+				</view>
+				<view class="u-content">
+					<u-parse :html="content"></u-parse>
+				</view>
+			</view>
+		</view>
+		<view class="footer">
+			<view class="item">
+				<view class="icon">
+					<u-icon name="home" color="#666" size="44"></u-icon>
+				</view>
+				<view class="text">
+					首页
+				</view>
+			</view>
+			<view class="item">
+				<view class="icon">
+					<u-icon name="kefu-ermai" color="#666" size="44"></u-icon>
+				</view>
+				<view class="text">
+					客服
+				</view>
+			</view>
+			<view class="item">
+				<view class="icon">
+					<u-icon name="heart" color="#666" size="44"></u-icon>
+				</view>
+				<view class="text">
+					收藏
+				</view>
+			</view>
+			<view class="item">
+				<view class="icon">
+					<u-badge  bgColor="#ff1f44" size="10" :offset="[-5,-10]"  count="10"></u-badge>
+					<u-icon name="shopping-cart" color="#666" size="44"></u-icon>
+				</view>
+				<view class="text">
+					购物车
+				</view>
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				content: `
+					<p>露从今夜白，月是故乡明</p>
+					<img src="https://cdn.uviewui.com/uview/swiper/2.jpg" />
+				`,
+				list: [{
+						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+	.wrapper{
+		padding: 30upx;
+		box-sizing: border-box;
+		.share{
+			display: flex;
+			align-items: center;
+			.money{
+				font-size:45upx;
+				color: #E34819;
+				font-weight: 700;
+				margin-right: 20upx;
+			}
+			.vip-money{
+				font-size:28upx;
+				color: #282828;
+				font-weight: 700;
+			}
+			.vip-icon{
+				margin-left: 10upx;
+				width: 50upx;
+				height: 22upx;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+		.introduce{
+			font-size: 34upx;
+			margin-top: 20upx;
+			font-weight: 700;
+		}
+		.label{
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			font-size: 24upx;
+			color: #82848f;
+			margin-top: 20upx;
+		}
+	}
+	.line{
+		width: 100%;
+		height: 20upx;
+		background-color: #f5f5f5;
+	}
+	.attribute{
+		height: 88upx;
+		background-color: #FFFFFF;
+		padding: 30upx;
+		box-sizing: border-box;
+		font-size: 24upx;
+		color: #82848f;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.attribute-left{
+			display: flex;
+			align-items: center;
+			
+		}
+	}
+	.UserEvaluation{
+		height: 88upx;
+		background-color: #FFFFFF;
+		padding: 30upx;
+		box-sizing: border-box;
+		font-size: 24upx;
+		color: #82848f;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.UserEvaluation-left{
+			font-size: 28upx;
+			color: #282828;
+			
+		}
+		.UserEvaluation-right{
+			display: flex;
+			align-items: center;
+			color: grey;
+		}
+	}
+	.shop-detail{
+		padding: 30upx 0upx;
+		.title{
+			font-size: 30upx;
+			color: #282828;
+			text-align: center;
+		}
+		.u-content {
+			margin-top: 30upx;
+		}
+	}
+	.footer{
+		padding: 10upx 30upx;
+		background-color: #FFFFFF;
+		display: flex;
+		align-items: center;
+		position: fixed;
+		bottom: 0upx;
+		z-index: 1;
+		width: 100%;
+		box-shadow: 0upx 0upx 10upx #e3e3e3;
+		.item{
+			 margin-right: 40upx;
+			 position: relative;
+			.icon{
+				width: 44upx;
+				height: 44upx;
+				margin: 0 auto;
+				display: flex;
+				justify-content: center;
+			}
+			.text{
+				font-size: 24upx;
+				text-align: center;
+				color: #666;
+			}
+		}
+	}
+</style>
